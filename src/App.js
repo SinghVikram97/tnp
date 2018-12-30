d74b2a1ec5a38056f3ec3e53794e0ca8199ea455
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2018-12-30 23:56:30 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar'
 import './App.css';
 import '../node_modules/tachyons/css/tachyons.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="tc f1 b">
-        Hi
+      <div>
+        <Navbar/>
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="zone">
+        <div id="collegeName">
+          Delhi Technological University
+        </div>
+        <ul className="main-nav">
+          <li class="img">
+            <img alt="logo"src="http://i65.tinypic.com/2rzys0x.png">
+            </img>
+          </li>
+          <li class="logoHeading">Students Placement Office</li>
+          <li><a href="ww">Why Recruit?</a><img alt="dropDown" src="http://i63.tinypic.com/a10f1c.png"></img></li>
+          <li><a href="ww">For Companies</a><img alt="dropDown" src="http://i63.tinypic.com/a10f1c.png"></img></li>
+          <li><a href="ww">For Students</a><img alt="dropDown" src="http://i63.tinypic.com/a10f1c.png"></img></li>
+          <li><a href="ww">Contact</a><img alt="dropDown" src="http://i63.tinypic.com/a10f1c.png"></img></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
